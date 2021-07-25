36003165cb4f5d1150b5e171aa2352fb68810753
jianghjs <jianghjs@foxmail.com>
2021-07-25 19:41:08 +0800

    1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,5 +5,29 @@
       We are here to help! Please read the <a> F.A.Q</a> first, and if you don'
       t find the answer to your question, <a> send us a ticket!</a>
     </p>
+    
+    <el-button @click="dialogVisible = true">popup</el-button>
+
+    <el-dialog title="tip" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+      <span>message</span>
+    </el-dialog>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible : false
+    }
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm("is close?")
+      .then(_=> {
+        done();
+      }).catch(_=>{});
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import "babel-polyfill"
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import AppLayout from './components/AppLayout'
 import router from './router'
 import VueFetch from './plugins/fetch'
@@ -7,7 +9,9 @@ import './global-components'
 
 Vue.use(VueFetch, {
   baseUrl: 'http://localhost:3000/'
-})
+});
+
+Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
